b4af46c825a770e362a12d2512158e7566a718ba
cpguillo <cpguillo@zone06ec.(none)>
2013-04-24 17:02:23 -0600

    mid report

--- results/chessPlot.m
+++ results/chessPlot.m
@@ -1,5 +1,5 @@
-data = load ("history_individuals_2.txt");
-iterations = 216;
+data = load ("history_individuals.txt");
+iterations = 4004;
 v = [1:iterations];
 hold on;
 xlabel ("O(iterations)");
@@ -13,4 +13,4 @@ title ("Evolution of parameters through iterations");
 plot (v, data (:,2), "r", v, data (:,3), "g", v, data (:,4), "b",v, data (:,5), "m", v, data (:,6), "c");
 legend ({"Pawn", "Knight", "Bishop", "Rook", "Queen"});
 %legend('show');
-print("history_individuals_2.png")
+print("history_individuals.png")
